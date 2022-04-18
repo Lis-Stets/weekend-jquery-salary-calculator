f445161fa446597b4ad9a2428d8ab25ff5983e66
Lisette Stetson <lisette.stetson@gmail.com>
2022-04-17 22:35:00 -0400

    added the display inputs function

--- scripts/client.js
+++ scripts/client.js
@@ -7,9 +7,10 @@ function onReady(){
 
 let employees = [];
 
+//collects _employee first name, last name, ID number, job title, annual salary_. 
 function newEmployee(){
     console.log('in newEmployee' );
-    let newEmployee = {
+    let employeeToAdd = {
         firstName: $( '#firstNameInput' ).val(),
         lastName: $( '#lastNameInput' ).val(),
         idNumber: $( '#idNumberInput' ).val(),
@@ -17,17 +18,37 @@ function newEmployee(){
         annualSalary: $( '#annualSalaryInput' ).val(),
 
     }
+    employees.push( employeeToAdd );
+    //clear the input fields (js)
+    $( '#firstNameInput' ).val( '' );
+    $( '#lastNameInput' ).val( '' );
+    $( '#idNumberInput' ).val( '' );
+    $( '#jobTitieInput' ).val( '' );
+    $( '#annualSalaryInput' ).val( '' );
 
+    displayInputs( employees );
 }
+console.log( employees );
 
-//collects _employee first name, last name, ID number, job title, annual salary_. 
-
-// A 'Submit' button should collect the form information, (html, js)
-//store the information (js)
 //append information to the DOM (html, jq?)
-//clear the input fields (js)
 //calculate monthly costs (js)
 //append this to the to DOM. (html, jq)
-//If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost. (js, css)
 
+function displayInputs( inputArray ){
+    console.log( 'in displayInputs' );
+    let unorderedListItemToBeDisplayed = $( '#employeeListOutput' );
+    unorderedListItemToBeDisplayed.empty();
+    let totalMonthlyCost = 0;
+
+    for( let i=0;i<inputArray.length; i++ ){
+        unorderedListItemToBeDisplayed.append(`<li>${inputArray[i].firstName} ${inputArray[i].lastName}: ${inputArray[i].idNumber}, ${inputArray[i].jobTitie}, $${inputArray[i].annualSalary}`);
+        totalMonthlyCost += Number( inputArray[i].annualSalary ).toFixed( 2 )/12;
+        console.log( totalMonthlyCost );
+    }
+    $('#totalMonthlyCostOutput').empty();
+    $('#totalMonthlyCostOutput').append( totalMonthlyCost )
+}
+
+
+//If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost. (js, css)
 // Create a delete button that removes an employee from the DOM.(Jq)
