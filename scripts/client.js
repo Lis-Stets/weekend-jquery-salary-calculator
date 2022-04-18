50b865a81882540aea45bdbc8337c945fdc3cdd8
Lisette Stetson <lisette.stetson@gmail.com>
2022-04-18 15:03:50 -0400

    added remove employee button and functionality

--- scripts/client.js
+++ scripts/client.js
@@ -3,7 +3,8 @@ $( document ).ready( onReady );
 function onReady(){
     $( console.log( "Ready." ) );
     $( '#submitInputsbutton' ).on( 'click', newEmployee );
-}
+    $( '#employeeListOutput' ).on( 'click', '.removeEmployeeButton', removeEmployee )
+}//end onReady
 
 let employees = [];
 
@@ -27,13 +28,12 @@ function newEmployee(){
     $( '#annualSalaryInput' ).val( '' );
 
     displayInputs( employees );
-}
+}// end newEmployee
 console.log( employees );
 
 //append information to the DOM (html, jq?)
 //calculate monthly costs (js)
 //append this to the to DOM. (html, jq)
-
 function displayInputs( inputArray ){
     console.log( 'in displayInputs' );
     let unorderedListItemToBeDisplayed = $( '#employeeListOutput' );
@@ -41,7 +41,7 @@ function displayInputs( inputArray ){
     let totalMonthlyCost = 0;
 
     for( let i=0;i<inputArray.length; i++ ){
-        unorderedListItemToBeDisplayed.append(`<li>${inputArray[i].firstName} ${inputArray[i].lastName}: ${inputArray[i].idNumber}, ${inputArray[i].jobTitie}, $${inputArray[i].annualSalary}`);
+        unorderedListItemToBeDisplayed.append(`<li>${inputArray[i].firstName} ${inputArray[i].lastName}: ${inputArray[i].idNumber}, ${inputArray[i].jobTitie}, $${inputArray[i].annualSalary}  <button id="removeEmployeeButton" class="removeEmployeeButton" data-index= "${i}">Remove Employee</button> </li>`);
         totalMonthlyCost += Number( inputArray[i].annualSalary ).toFixed( 2 )/12;
         console.log( totalMonthlyCost );
     }
@@ -51,9 +51,15 @@ function displayInputs( inputArray ){
     if( totalMonthlyCost > 20000){
         $( '#totalMonthlyCostOutput' ).css( 'background-color', 'rgba(255, 0, 0, 0.863)');
     }
-    else{rgb(230, 250, 231)
+    else{
         $( '#totalMonthlyCostOutput' ).css( 'background-color', 'rgb(230, 250, 231)');
     }
 }
 
 // Create a delete button that removes an employee from the DOM.(Jq)
+function removeEmployee(){
+    //$( this ).parent().remove();
+    console.log( $( this ).data( 'index' ) );
+    employees.splice( $( this ).data( 'index' ), 1 );
+    displayInputs( employees );
+}
